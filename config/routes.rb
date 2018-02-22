8a05cf55af4fec53cd46728531a99332e590db75
yujunk <yujunk@gmail.com>
2018-02-22 17:58:42 +0800

    Dummy test braintree integration on ruby on rails app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'braintree/new'
+
+  get 'view/new'
+
+  root 'welcome#index'
+
+  resource :braintree, controller: "braintree"
+
+  post 'braintree/checkout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
